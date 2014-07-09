4ca86c9ae9ad13561558461c7e4bb62dfa96efac
Yousaf Ehsan <mmesunny@gmail.com>
2014-07-09 12:16:10 +0100

    added majid's html to ci

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,11 +1,10 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Home extends CI_Controller {
 
-//majid //
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('home_view');
 	}
 }
 
---
+++ application/views/home_view.php
@@ -0,0 +1,218 @@
+<html>
+<head>
+<title></title>
+<meta name="" content="">
+</head>
+<body>
+
+<style type="text/css" style = "display:none;">
+	
+	
+	.header
+	{
+		width:80%;
+		margin-left:10%;
+		border:solid 1px;
+		height:70px;
+		background-color:#5c5e55;
+		color:white;
+	}
+	.web_name1
+	{
+	    height: 35px;
+		float: left;
+		width:100%;
+		line-height:35px;
+		font-size:20px;
+		margin-left:20px;
+	}
+	.under_name
+	{
+		height: 35px;
+		float:left;
+		width:100%;
+	}
+	.web_name2
+	{
+		margin-left:20px;
+		height: 35px;
+		float: left;
+		width:20%;
+		line-height:35px;
+		float:left;
+		font-size:20px;
+	}
+	.navigation_box
+	{
+		width: 80%;
+		margin-left:20%;
+	}
+	.navigate_buttons
+	{
+		float:left;
+		width: 60px;
+		height:35px;
+		line-height: 35px;
+		margin-right: 10px;
+		text-align: center;
+		font-size: 20px;
+	}
+	.navig_dividors
+	{
+		height: 35px;
+		width:20px;
+		line-height:35px;
+		text-align:center;
+		font-size: 20px;
+		float:left;
+		font-weight:bold;
+	}
+	.box_size
+	{
+		height:350px;
+		width:50%;
+		margin-left:25%;
+		border-radius: 3px;
+		margin-top:30px;
+		float:left;
+	}
+	.landscape
+	{
+		width: 100%;
+		height:250px;
+	    border-radius: 3px;
+		margin-bottom:25px;
+		float:left;
+	}
+	.landscape_tiny_box
+	{
+		width:24%;
+		background-color:#2fdf20;
+		height:70px;
+		float:left;
+		border-radius: 3px;
+		line-height:70px;
+		text-align:center;
+	}
+	.tiny_separator
+	{
+		width:9px; background-color: white;height: 70px; float: left;
+		border-radius: 3px;
+	}
+	.landscape_header
+	{
+		height:35px;
+		width:100%;
+		text-align:center;
+		line-height:35px;
+		background-color:#8f9897;
+		border-radius: 3px;
+	}
+	hr
+	{
+		width: 25%;
+	}
+	.pics_box
+	{
+		width:100%;
+		height:215px;
+		margin-top:30px;
+		border-radius: 3px;
+	}
+	.pic_box
+	{
+		width:24%;
+		background-color:#2fdf20;
+		height:170px;
+		float:left;
+		border-radius: 3px;
+	}
+	.for_pic
+	{
+		height:170px;
+	}
+	.pic_img
+	{
+		width:100%;
+		height:170px;
+		float:left;
+	}
+</style>
+
+<div class = "header">
+	<div class = "web_name1">Karachi</div>
+	<div class = "under_name">
+		<div class = "web_name2"> Camera Center </div>
+		<div class = "navigation_box"> 
+			  <div class = "navigate_buttons" style="margin-left: 150px;">Home</div>
+			  <div class = "navig_dividors">/</div>
+			  <div class = "navigate_buttons">Shop</div>
+			  <div class = "navig_dividors">/</div>
+			  <div class = "navigate_buttons">About</div>
+			  <div class = "navig_dividors">/</div> 
+			  <div class = "navigate_buttons">Blog</div>
+	   </div>
+	</div>
+</div>
+
+
+<div class="black_box box_size">
+	<div class = "landscape" style="background-color: #0b0000;"></div>
+	<div class = "landscape_tiny_box" ></div><div class = "tiny_separator"></div>
+	<div class = "landscape_tiny_box"></div><div class = "tiny_separator"></div>
+	<div class = "landscape_tiny_box"></div><div class = "tiny_separator"></div>
+	<div class = "landscape_tiny_box"></div>
+</div>
+<div class="catagory_box box_size">
+	<div  class = "landscape">
+		
+	   <div class = "landscape_header"> <hr style="float: left; margin-top:15px;"> Header Name <hr style="float: right; margin-top:15px;">  </div>
+		<div class="pics_box">
+			<div class="pic_box"> <img class="pic_img" src="" alt="" /></div><div class = "tiny_separator for_pic"></div>
+			<div class="pic_box"> <img class="pic_img" src="" alt="" /></div><div class = "tiny_separator for_pic"></div>
+			<div class="pic_box"> <img class="pic_img" src="" alt="" /></div><div class = "tiny_separator for_pic"></div>
+			<div class="pic_box"> <img class="pic_img" src="" alt="" /></div>
+		</div>
+	</div>
+	<div class = "landscape_tiny_box"></div><div class = "tiny_separator"></div>
+	<div class = "landscape_tiny_box"></div><div class = "tiny_separator"></div>
+	<div class = "landscape_tiny_box"></div><div class = "tiny_separator"></div>
+	<div class = "landscape_tiny_box"></div>
+</div>
+<div class="latest box_size">
+	<div  class = "landscape">
+		
+		<div class = "landscape_header"> <hr style="float: left; margin-top:15px;"> Header Name <hr style="float: right; margin-top:15px;">  </div>
+		<div class="pics_box">
+			<div class="pic_box"> <img class="pic_img" src="" alt="" /></div><div class = "tiny_separator for_pic"></div>
+			<div class="pic_box"> <img class="pic_img" src="" alt="" /></div><div class = "tiny_separator for_pic"></div>
+			<div class="pic_box"> <img class="pic_img" src="" alt="" /></div><div class = "tiny_separator for_pic"></div>
+			<div class="pic_box"> <img class="pic_img" src="" alt="" /></div>
+		</div>
+		</div>
+	<div class = "landscape_tiny_box"></div><div class = "tiny_separator"></div>
+	<div class = "landscape_tiny_box"></div><div class = "tiny_separator"></div>
+	<div class = "landscape_tiny_box"></div><div class = "tiny_separator"></div>
+	<div class = "landscape_tiny_box"></div>
+</div>
+<div class="blog box_size">
+	<div class = "landscape">
+		
+		<div class = "landscape_header"> <hr style="float: left; margin-top:15px;"> Header Name <hr style="float: right; margin-top:15px;">  </div>
+		
+		<div class="pics_box">
+			<div class="pic_box"> <img class="pic_img" src="" alt="" /> </div><div class = "tiny_separator for_pic"></div>
+			<div class="pic_box"> <img class="pic_img" src="" alt="" /></div><div class = "tiny_separator for_pic"></div>
+			<div class="pic_box"> <img class="pic_img" src="" alt="" /></div><div class = "tiny_separator for_pic"></div>
+			<div class="pic_box"> <img class="pic_img" src="" alt="" /></div>
+		</div>
+		</div>
+		
+	
+	<div class = "landscape_tiny_box"></div><div class = "tiny_separator"></div>
+	<div class = "landscape_tiny_box"></div><div class = "tiny_separator"></div>
+	<div class = "landscape_tiny_box"></div><div class = "tiny_separator"></div>
+	<div class = "landscape_tiny_box"></div>
+</div>
+</body>
+</html>
