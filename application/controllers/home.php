d3d8d5828ec20bc65f7c513f2fa0f3658cefa5ef
Yousaf Ehsan <mmesunny@gmail.com>
2014-07-09 12:29:20 +0100

    Login page

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,10 +1,22 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Home extends CI_Controller {
+	public function __construct(){
+		parent::__construct();
+	}
 
 	public function index()
 	{
 		$this->load->view('home_view');
+	}
+
+	public function login(){
+		$page_title = "Login Page";
+		$data = array(
+			"page_title"=> $page_title
+
+			);
+		$this->load->view('login_view',$data);
 	}
 }
 
---
+++ application/views/login_view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo $page_title?></title>
+</head>
+<body>
+<form>
+	Username:<input type="text"><br>
+	Password:<input type="password"><br>
+	<input type="submit">
+</form>
+</body>
+</html>
