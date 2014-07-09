34acb324c04447d472bb29ba340c9df1488b093a
majidlateef93 <majid.lateef.93@gmail.com>
2014-07-09 17:38:52 +0500

    Register page

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,26 +1,72 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
-class Home extends CI_Controller {
-	public function __construct(){
-		parent::__construct();
-	}
+class Home extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-	public function index()
-	{
-		$this->load->view('home_view');
-	}
+    public function index()
+    {
+        $this->load->view('home_view');
+    }
 
-	public function login(){
-		$page_title = "Login Page";
-		$data = array(
-			"page_title"=> $page_title
+    public function login()
+    {
+        $page_title = "Login Page";
+        $data = array("page_title" => $page_title);
+        $this->load->view('login_view', $data);
+    }
 
-			);
-		$this->load->view('login_view',$data);
-	}
+    public function register()
+    {
+        $this->load->view('register_view');
+    }
+    public function register_request()
+    {
+        $username = $this->input->post('username', true);
+        $password = $this->input->post('password', true);
+        $password2 = $this->input->post('password2', true);
+        $email = $this->input->post('email', true);
+        $mobile = $this->input->post('mobile', true);
+        $address = $this->input->post('address', true);
+        if (empty($username) || empty($password) || empty($password2) || empty($email) ||
+            empty($mobile) || empty($address)) {
+            echo json_encode(array('error' => 'Please fill in required fields'));
+        } elseif (!filter_var($email, VALIDATE_EMAIL)) {
+            echo json_encode(array('error' => 'Email is not valid'));
+        } elseif ($password != $password2) {
+            echo json_encode(array('error' => 'password doesnot matched'));
+        } elseif(!is_valid_mobile_number($mobile)){
+            echo json_encode(array('error' => 'Mobile number is not valid'));
+        }else{// register the user
+            
+        }
+    }
 
-	public function register(){
-		
-	}
+
+    private function is_valid_mobile_number($telNumber)
+    {
+        if (!is_string($telNumber) && !is_int($telNumber))
+            return false;
+
+        /* All regex patterns written by Sag-e-Attar Junaid Atari */
+        $patterns = array( // 'premiumNumber'  => '/^0(8|9)00 ?[0-9]{3} ?[0-9]{2}$/',
+
+            'mobileNumber' => '/^((\(((\+|00)92)\)|(\+|00)92)(( |\-)?)' . '(3[0-9]{2})\6|0(3[0-9]{2})( |\-)?)[0-9]' .
+                '{3}( |\-)?[0-9]{4}$/'
+                // 'landlineNumber' => '/^(\((\+|00)92\)( )?|(\+|00)92( )?|0)' .
+                //                            '[1-24-9]([0-9]{1}( )?[0-9]{3}( )?[0-9]' .
+            //                            '{3}( )?[0-9]{1,2}|[0-9]{2}( )?[0-9]{3}' .
+            //                            '( )?[0-9]{3})$/'
+            );
+
+        foreach ($patterns as $pattern)
+            $response[] = preg_match($pattern, $telNumber) ? true : false;
+
+        return ($response[0]);
+    }
+
 }
-
---
+++ application/views/register_view.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo $page_title?></title>
+</head>
+<body>
+<form id="register_form">
+	Username:<input name="username" type="text"><br>
+	Password:<input name="password" type="password"><br>
+    Password confirm<input name="password2" type="password"><br>
+    email:<input name="email" type="email"><br>
+    mobile No:<input name="mobile" type="number"><br>
+    address:<input name="address" type="text"><br>
+
+	<input id="register_btn" type="submit">
+</form>
+<div id="message"></div>
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script>
+$(document).ready(function(){
+    // register user
+    $("#resgister_btn").click(function(e){
+        e.preventDefault();
+        $.ajax({
+            url :"",
+            type:"POST",
+            data:$("#register_form").serialize(),
+            dataType:"json",
+            success:function(data){
+                if(typeof(data.error) == "undefined"){
+                    $("#message").text(data.success);
+                    $("#message").css("color","green");
+                }else{
+                    $("#message").text(data.error);
+                      $("#message").css("color","red");
+                }
+            }
+            
+        });
+
+    });// resgiter function ends
+    
+});// document ready ends
+</script>
+</body>
+</html>
