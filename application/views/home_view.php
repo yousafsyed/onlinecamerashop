c0ade929d4d174ef0f2f91c386212bfe0c60130e
yousafsyed <mmesunny@gmail.com>
2014-07-19 19:52:11 +0100

    Updates

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -3,33 +3,111 @@
 <title></title>
 <meta name="" content="">
 <link rel="stylesheet" type="text/css" href="<?=base_url('public/style.css')?>">
+<link rel="stylesheet" type="text/css" href="<?=base_url('public/css/bootstrap.min.css')?>">
+<link rel="stylesheet" type="text/css" href="<?=base_url('public/css/bootstrap-theme.min.css"')?>">
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<script>
+$(document).ready(function(){
+    // login user
+    $("#login_btn").click(function(e){
+    	$("#login_btn").attr('disabled','disabled');
+    	$("#login_btn").text('Processing');
+        e.preventDefault();
+        $.ajax({
+            url :"<?=base_url('index.php/home/login_request')?>",
+            type:"POST",
+            data:$("#login_form").serialize(),
+            dataType:"json",
+            success:function(data){
+            	$("#login_btn").removeAttr('disabled');
+            	$("#login_btn").text('Submit');
+                if(typeof(data.error) == "undefined"){
+                    setTimeout(function(){
+                        window.location = "<?=base_url('index.php/home')?>"
+                    },500);
+                }else{
+                    $("#message").show();
+                    $("#message .messageclass").text(data.error);
+                }
+            }
+
+        });
+
+    });// login function ends
+
+
+
+
+});// document ready ends
+</script>
 </head>
 <body>
 
+<div id="message" style="display:none;margin:0;border-radius:0;" class="alert alert-danger fade in" role="alert">
 
-<div class="header">
-	
-    
-    <span class="web_name1">Karachi</span>
-	<span class="under_name">
-		
-		<span class="navigation_box"> 
-				  <nav class="mainNav">
-				    <ul>
-				<li><a href="#">
-				Home
-				</a></li>
-
-				<li><a href="#">
-				About
-				</a></li>
-
-				</ul></nav><a>
-				</a></span>
-	</span>
+      <span class="messageclass"></span>
 </div>
+<nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
 
-Hi, <?=$email;?>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link</a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+<?php if (!$logged_in) {?>
+	<form id="login_form" class="navbar-form navbar-right" role="login">
+						<div class="form-group">
+										<input type="text" name="useremail" class="form-control" placeholder="john@gmail.com">
+														<input type="password" name="userpassword"  class="form-control" placeholder="password">
+																													        </div>
+									<button type="submit" id="login_btn" class="btn btn-default">Submit</button>
+																													      </form>
+	<?php } else {?>
+																													<ul class="nav navbar-nav navbar-right">
+
+																													        <li class="dropdown">
+																													          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?=$user_name?><span class="caret"></span></a>
+																													          <ul class="dropdown-menu" role="menu">
+																													            <li><a href="#">Action</a></li>
+																													            <li><a href="#">Another action</a></li>
+																													            <li><a href="#">Something else here</a></li>
+																													            <li class="divider"></li>
+																													            <li><a href="<?=base_url('index.php/home/logout')?>">Logout</a></li>
+																													          </ul>
+																													        </li>
+																													      </ul>
+	<?php }?>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<div class="container">
+
 
 <div class="black_box box_size">
 	<div class = "landscape" style="background-color: #0b0000;"></div>
@@ -40,7 +118,7 @@ Hi, <?=$email;?>
 </div>
 <div class="catagory_box box_size">
 	<div  class = "landscape">
-		
+
 	   <div class = "landscape_header">Categories
 	     <hr style="float: right; margin-top:15px;">  </div>
 		<div class="pics_box">
@@ -57,7 +135,7 @@ Hi, <?=$email;?>
 </div>
 <div class="latest box_size">
 	<div  class = "landscape">
-		
+
 		<div class = "landscape_header"> Latest Products
 		  <hr style="float: right; margin-top:15px;">  </div>
 		<div class="pics_box">
@@ -74,10 +152,10 @@ Hi, <?=$email;?>
 </div>
 <div class="blog box_size">
 	<div class = "landscape">
-		
+
 		<div class = "landscape_header"> Latest Blog
 		  <hr style="float: right; margin-top:15px;">  </div>
-		
+
 		<div class="pics_box">
 			<div class="pic_box"> <img class="pic_img" src="" alt="" /> </div><div class = "tiny_separator for_pic"></div>
 			<div class="pic_box"> <img class="pic_img" src="" alt="" /></div><div class = "tiny_separator for_pic"></div>
@@ -85,12 +163,15 @@ Hi, <?=$email;?>
 			<div class="pic_box"> <img class="pic_img" src="" alt="" /></div>
 		</div>
 		</div>
-		
-	
+
+
 	<div class = "landscape_tiny_box"></div><div class = "tiny_separator"></div>
 	<div class = "landscape_tiny_box"></div><div class = "tiny_separator"></div>
 	<div class = "landscape_tiny_box"></div><div class = "tiny_separator"></div>
 	<div class = "landscape_tiny_box"></div>
+
+	</div>
+
 </div>
 </body>
 </html>
