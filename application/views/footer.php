9a209c05bd24c7bf5ab934f4bdd912c17b420eac
majidlateef93 <majid.lateef.93@gmail.com>
2014-10-15 19:30:19 +0500

    header and footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -13,23 +13,21 @@
 
 
 
-<div style="background:url();height:300px;width:100%;position: relative;text-align: center;vertical-align: top;">
-    <div style="display:inline-block;width:24%;border-right:1px solid #999;height:300px;vertical-align: top;">
-        <div class="Block CategoryList Moveable Panel">
-            <h2>Customer Service</h2>
+<div style="background:url();height:200px;width:100%;position: relative;text-align: center;vertical-align: top;  background-color: lightgrey;">
+    <div style="display:inline-block;width:24%;border-right:1px solid #999;height:200px;vertical-align: top;">
+        <div class="Block CategoryList Moveable Panel" style="background-color: lightgrey;">
+            <h2>Categories</h2>
             <div class="BlockContent">
                 <ul style="list-style: none; text-align: left; >
-                    <li><a href="" title="Contact Us" target="_blank">Contact Us</a></li>
-                    <li><a href="" title="How to Order" target="_blank">How to Order</a></li>
-                    <li><a href="" title="Shipping Details!" target="_blank">Shipping Details!</a></li>
-                    <li><a href="" title="Warrenty &amp; Returns" target="_blank">Warranty &amp; Returns</a></li>
-                    <li><a href="" title="Support Tickets!" target="_blank">Support Tickets!</a></li>
-                </ul>
+                    <li><a href="" title="Contact Us" target="_blank">Bath &amp; SPA</a></li>
+                    <li><a href="" title="How to Order" target="_blank">Natural Hair Make Up</a></li>
+                    <li><a href="" title="Warrenty &amp; Returns" target="_blank">Organic Skin Care </a></li>
+                     </ul>
             </div>
         </div>
     </div>
-    <div style="display:inline-block;width:24%;border-right:1px solid #999;height:300px;vertical-align: top;">
-    <div class="Block CategoryList Moveable Panel">
+    <div style="display:inline-block;width:24%;border-right:1px solid #999;height:200px;vertical-align: top;background-color: lightgrey;">
+    <div class="Block CategoryList Moveable Panel" style="background-color: lightgrey;">
             <h2>Store Information</h2>
             <div class="BlockContent">
                 <ul style="list-style: none; text-align: left; " >
@@ -42,9 +40,21 @@
             </div>
         </div>
     </div>
-    <div style="display:inline-block;width:24%;border-right:1px solid #999;height:300px;vertical-align: top;">
-    a</div>
-    <div style="display:inline-block;width:24%;border-right:1px solid #999;height:300px;vertical-align: top;"><div class="Block CategoryList Moveable Panel">
+    <div style="display:inline-block;width:24%;border-right:1px solid #999;height:200px;vertical-align: top; background-color: lightgrey;">
+    <div class="Block CategoryList Moveable Panel" style="background-color: lightgrey;">
+            <h2>Store Information</h2>
+            <div class="BlockContent">
+                <ul style="list-style: none; text-align: left; " >
+                    <li><a href="" title="Contact Us" target="_blank">Contact Us</a></li>
+                    <li><a href="" title="How to Order" target="_blank">How to Order</a></li>
+                    <li><a href="" title="Shipping Details!" target="_blank">Shipping Details!</a></li>
+                    <li><a href="" title="Warrenty &amp; Returns" target="_blank">Warranty &amp; Returns</a></li>
+                    <li><a href="" style="alignment-adjust: after-edge;" title="Support Tickets!" target="_blank">Support Tickets!</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div style="display:inline-block;width:24%;border-right:1px solid #999;height:200px;vertical-align: top;"><div class="Block CategoryList Moveable Panel" style="background-color: lightgrey;">
                       <h2>Payment Methods</h2>
                     <div class="BlockContent fContact">
 
--- application/views/header.php
+++ application/views/header.php
@@ -6,8 +6,33 @@ error_reporting(0);
 
 
 
+<div style="height:120px;  background-color: rgb(116, 119, 129);">
+    <div style="display: inline-block; width: 50%;">
+        <img src="<?= base_url() ?>public/images/tiny/others.jpg" style="height:100px;padding: 10px;margin-left: 20px;"/>
+    </div>
+    <?php
+    if (!$this->session->userdata("user_id")):?>
+    <div style="display:inline-block;float: right; padding:20px">
+        <form id="login_form" class="navbar-form navbar-right" role="login">
+        		<div class="form-group">
+        		<input type="text" name="useremail" class="form-control" placeholder="Majid@gmail.com">
+        		<input type="password" name="userpassword" class="form-control" placeholder="type password">
+        		<button type="submit" id="login_btn" class="btn btn-default">Submit</button>
+                </div>
+        	</form>
+            
+            <div ><div style="float:right;Width:85px">
+                <a href="<?php echo base_url('')?>" style="padding:10px;color:White">Register</a>
+            </div>
+            </div>
+    </div>
+    
+    <?php
+        endif;
+    ?>
+</div>
 
-<nav class="navbar navbar-default navbar-fixed-top" role="navigation" style="float: left; margin-bottom: 80px !important;">
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation" style="/* float: left; */ margin-bottom: 80px !important;position: initial;width: 100%;">
 
 <!-- <div style="background-color: red; width: 100%; height: 40px; float: left;"> -->
 	
@@ -21,7 +46,6 @@ error_reporting(0);
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?=base_url()?>" style="font-weight: bold; color: blueviolet;">KaRACHI CAMERA CENTER</a>
     </div>
     
 
@@ -34,27 +58,19 @@ error_reporting(0);
         </li>
       </ul>
 
-      	<form class="navbar-form navbar-left" role="Search">
-						<div class="form-group">
-						<input type="text" name="q" class="form-control" placeholder="Search">
+      	<form class="navbar-form navbar-left" style="width: 40%;" role="Search">
+						<div class="form-group" style="width: 80%;">
+						<input type="text" name="q" style="width:100%;" class="form-control" placeholder="Search">
 
 						</div>
 					<button type="submit"  class="btn btn-default">Submit</button>
 	</form>
 <?php
-if (!$this->session->userdata("user_id")):?>
-<form id="login_form" class="navbar-form navbar-right" role="login">
-		<div class="form-group">
-		<input type="text" name="useremail" class="form-control" placeholder="Majid@gmail.com">
-		<input type="password" name="userpassword" class="form-control" placeholder="type password">
-				<button type="submit" id="login_btn" class="btn btn-default">Submit</button>
-	</form>
-<?php
-else:?>
+if ($this->session->userdata("user_id")):?>
 <ul class="nav navbar-nav navbar-right">
 
 	<li class="dropdown">
-			<a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $this->session->userdata("user_name"); 
+			<a href="#" class="dropdown-toggle navbar-default" style="color: white; font-weight:bold; background-color: #e7e7e7;" data-toggle="dropdown"><?php echo $this->session->userdata("user_name"); 
 ?><span class="caret"></span></a>
 	<ul class="dropdown-menu" role="menu">
 		<li><a href="<?=base_url('index.php/home/orders');?>">My Orders</a></li>
